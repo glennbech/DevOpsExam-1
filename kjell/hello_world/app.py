f1e8ba78392b21fdf7530a510e646f5013879903
varunthileepan <varunthileepan@gmail.com>
2023-11-23 18:48:23 +0100

    New deployment attempt

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -2,18 +2,6 @@ import json
 import boto3
 import os
 
-# Denne koden kan også kjøres som en selvstendig applikasjon (Uten SAM) bare gjøre følgende
-# (dersom man har python på maskinen sin altså...)
-#
-# Instruksjoner for å kjøre ... (Kan sikkert lage container senere ..)
-#
-# pip3 install -r requirements.txt
-# python3 app.py
-#
-# Hilsen Kjell
-
-
-
 s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
