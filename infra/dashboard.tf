f969cccc969016d77d7fd8497e9738bfb616f653
EC2 Default User <ec2-user@ip-172-31-45-63.eu-west-1.compute.internal>
2023-11-28 03:26:38 +0000

    changed to JSON encode

--- infra/dashboard.tf
+++ infra/dashboard.tf
@@ -1,47 +1,39 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.dashboard_name
-  dashboard_body = <<DASHBOARD
-{
-  "widgets": [
-    {
-      "type": "metric",
-      "x": 0,
-      "y": 0,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.dashboard_name}",
-            "ppe.violation.person.count",
-            { "stat": "Sum", "period": 300 }
-          ]
-        ],
-        "period": 600,
-        "stat": "Average"
-        "region": "eu-west-1",
-        "title": "PPE Scan Count"
-      }
-    },
-    {
-      "type": "metric",
-      "x": 0,
-      "y": 6,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.dashboard_name}",
-            "image.size.avg",
-            { "stat": "Average", "period": 600 }
+  dashboard_body = jsonencode({
+    "widgets": [
+      {
+        "type": "metric",
+        "x": 0,
+        "y": 0,
+        "width": 12,
+        "height": 6,
+        "properties": {
+          "metrics": [
+            ["${var.dashboard_name}", "ppe.violation.person.count"]
           ],
-        ],
-        "region": "eu-west-1",
-        "title": "Image Size Metrics"
+          "period": 600,
+          "stat": "Average",
+          "region": "eu-west-1",
+          "title": "Number of people in images with violations"
+        }
+      },
+      {
+        "type": "metric",
+        "x": 12,
+        "y": 0,
+        "width": 12,
+        "height": 6,
+        "properties": {
+          "metrics": [
+            ["${var.dashboard_name}", "img.size.avg"]
+          ],
+          "period": 300,
+          "stat": "Average",
+          "region": "eu-west-1",
+          "title": "Average Image Size in S3 Bucket"
+        }
       }
-    }
-  ]
-}
-DASHBOARD
+    ]
+  })
 }
